149ea3ae0cd034124dc41b71aa793c1a145b5ca8
lmux <lmux@posteo.de>
2021-05-21 17:40:34 +0200

    updated url and added regions

--- get_srtm3_sdf.sh
+++ get_srtm3_sdf.sh
@@ -3,22 +3,28 @@
 #converts it to sdf files for Signal-Server
 #build srtm2sdf before using this script (./build srtm2sdf)
 
-url="https://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/"
+baseurl="https://web.archive.org/web/20191019205719/https://dds.cr.usgs.gov/srtm/version2_1/SRTM3/"
+region="Eurasia/"
+#region="Africa/"
+#region="Islands/"
+#region="North_America/"
+#region="South_America/"
+
 srtm3Dir="data/SRTM3/"
 srtm2sdfDir="utils/sdf/"
 
 #north bounds of height data to download
-for north in {44..55}
-do
+#for north in {44..55}
+#do
 	#east bounds of height data to download
-	for east in {010..019}
-	do
-	wget "${url}N${north}E$east.hgt.zip" -P $srtm3Dir
-	done
-done
+#	for east in {010..019}
+#	do
+#	wget "${baseurl}${region}N${north}E${east}.hgt.zip" -P $srtm3Dir
+#	done
+#done
 
-#get every listed .hgt.zip file in the server directory
-#wget $(lynx -dump -listonly $url | grep http |
+#get every listed .hgt.zip file in the server directory of the region
+#wget $(lynx -dump -listonly "${baseurl}${region}" | grep http |
 #	grep \.hgt.zip$ | awk '{print $2}') -P $srtm3Dir
 
 unzip "${srtm3Dir}*.hgt.zip" -d $srtm3Dir
@@ -29,4 +35,4 @@ do
 done
 
 mv *.sdf $srtm3Dir
-rm "${srtm3Dir}"*.hgt "${srtm3Dir}"*.hgt.zip
+#rm "${srtm3Dir}"*.hgt "${srtm3Dir}"*.hgt.zip
